4d1a177b3b67215e09d99a043f7c047ad05bb596
manuelSmo <manuel.smo24@gmail.com>
2023-01-04 17:27:49 -0500

    Handle all endpoint exceptions

--- src/main/java/com/manuel/contactos/controllers/ContactosController.java
+++ src/main/java/com/manuel/contactos/controllers/ContactosController.java
@@ -3,6 +3,8 @@ package com.manuel.contactos.controllers;
 import com.manuel.contactos.model.Contacto;
 import com.manuel.contactos.service.ContactoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,27 +19,39 @@ public class ContactosController {
     ContactoService contactoService;
 
     @GetMapping(value = "contactos", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Contacto> listarContactos(){
-        return contactoService.listarContactos();
+    public ResponseEntity<List<Contacto>> listarContactos(){
+        var listaContactos = contactoService.listarContactos();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("total", String.valueOf(listaContactos.size()));
+        return new ResponseEntity<List<Contacto>>(listaContactos,headers, HttpStatus.OK);
     }
 
     @GetMapping(value = "contactos/{correo}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Contacto buscarContacto(@PathVariable("correo") String correo){
-        return contactoService.buscarContacto(correo);
+    public ResponseEntity<Contacto> buscarContacto(@PathVariable("correo") String correo){
+        return new ResponseEntity<Contacto>(contactoService.buscarContacto(correo),HttpStatus.OK);
     }
 
     @PostMapping(value = "contactos/agregar", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void agregarContacto(@RequestBody Contacto contacto) throws Exception {
+    public ResponseEntity<Void> agregarContacto(@RequestBody Contacto contacto) throws Exception {
         contactoService.agregarContacto(contacto);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping(value = "contactos/actualizar", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public boolean actualizarContacto(@RequestBody Contacto contacto){
-        return contactoService.actualizarContacto(contacto);
+    public ResponseEntity<Void> actualizarContacto(@RequestBody Contacto contacto){
+        if (contactoService.actualizarContacto(contacto)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
 
     @DeleteMapping(value = "contactos/eliminar/{id}")
-    public boolean eliminarContacto(@PathVariable("id") int idContacto){
-        return contactoService.eliminarContacto(idContacto);
+    public ResponseEntity<Void> eliminarContacto(@PathVariable("id") int idContacto){
+        if (contactoService.eliminarContacto(idContacto)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
 }
